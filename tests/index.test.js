77676a6d7423c94f87c72922d6376ceb59802c27
Kirill A <burt.tsiatis@gmail.com>
2021-06-30 17:23:19 +0300

    imp: change by design

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,70 +10,70 @@ function range(start, stop, step) {
 
 describe('Test convertToRange', () => {
 
-  test('#empty in array', async () => {
+  test('#empty in array', () => {
     const arr = []
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('')
   })
 
-  test('#different ranges for [1,2,3,4,5,6,7,8]', async () => {
+  test('#different ranges for [1,2,3,4,5,6,7,8]', () => {
     const arr = range(1, 8)
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1-8')
   })
-  test('#different ranges for [1,3,4,5,6,7,8]', async () => {
+  test('#different ranges for [1,3,4,5,6,7,8]', () => {
     const arr = [1].concat(range(3, 8))
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1,3-8')
   })
-  test('#different ranges for [1,3,4,5,6,7,8,10,11,12]', async () => {
+  test('#different ranges for [1,3,4,5,6,7,8,10,11,12]', () => {
     const arr = [1].concat(range(3, 8)).concat(range(10, 12))
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1,3-8,10-12')
   })
-  test('#different ranges for [1,2,3', async () => {
+  test('#different ranges for [1,2,3', () => {
     const arr = range(1, 3)
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1-3')
   })
-  test('#different ranges for [1,2]', async () => {
+  test('#different ranges for [1,2]', () => {
     const arr = range(1, 2)
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1,2')
   })
-  test('#different ranges for [1,2,4]', async () => {
+  test('#different ranges for [1,2,4]', () => {
     const arr = [1,2,4]
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1,2,4')
   })
-  test('#different ranges for [1,2,4,5,6]', async () => {
+  test('#different ranges for [1,2,4,5,6]', () => {
     const arr = [1,2,4,5,6]
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1,2,4-6')
   })
-  test('#different ranges for [1,2,3,7,8,9,15,17,19,20,21]', async () => {
+  test('#different ranges for [1,2,3,7,8,9,15,17,19,20,21]', () => {
     const arr = [1,2,3,7,8,9,15,17,19,20,21] 
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1-3,7-9,15,17,19-21')
   })
-  test('#different ranges for [1,2,3,4,5,6,100,1091,1999,2000,2001,2002]', async () => {
+  test('#different ranges for [1,2,3,4,5,6,100,1091,1999,2000,2001,2002]', () => {
     const arr = [1,2,3,4,5,6,100,1091,1999,2000,2001,2002]
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1-6,100,1091,1999-2002')
   })
-  test('#different ranges for [1]', async () => {
+  test('#different ranges for [1]', () => {
     const arr = [1]  
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1')
   })
-  test('#different ranges for [1,3,5,7,9,11]', async () => {
+  test('#different ranges for [1,3,5,7,9,11]', () => {
     const arr = [1,3,5,7,9,11]  
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1,3,5,7,9,11')
   })
-  test('#string in array', async () => {
+  test('#string in array', () => {
     const arr = [1,null,3,'e',4].concat(range(6, 9))
-    const data = await convertToRange(arr)
+    const data = convertToRange(arr)
     expect(data).toBe('1,3,4,6-9')
   })
 
